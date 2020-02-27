292c7a386bc41ff1b8b2be1c8935f807bf04ddf5
Jose L Delgado Ruiz <fullmetalpepillo@gmail.com>
2020-02-27 06:11:08 -0800

    2 - Templates

--- flaskblog.py
+++ flaskblog.py
@@ -1,20 +1,36 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template, url_for
 
-#Python Flask Tutorial: Full-Featured Web App Part 1 - Getting Started
-##https://www.youtube.com/watch?v=MwZwr5Tvyxo&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=2&t=1s
+#Python Flask Tutorial: Full-Featured Web App Part 2 - Templates
+##https://www.youtube.com/watch?v=QnDWIZuWYW0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=2
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Larry Ullman',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018',
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018',
+    },
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    #name = request.args.get("name", "World")
+    #return f'Hello, {escape(name)}!'
     #return "Hello World!"
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return "About Page"
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
