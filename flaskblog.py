d3855183816bd28d8bec84098a51dacd8ed0969e
Jose L Delgado Ruiz <fullmetalpepillo@gmail.com>
2020-02-27 19:18:29 -0800

    3 - Forms and User Input

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,13 @@
-from flask import Flask, escape, request, render_template, url_for
+from flask import Flask, escape, request, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
-#Python Flask Tutorial: Full-Featured Web App Part 2 - Templates
-##https://www.youtube.com/watch?v=QnDWIZuWYW0&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=2
+#Python Flask Tutorial: Full-Featured Web App Part 3 - Forms and User Input
+##https://www.youtube.com/watch?v=UIJKdCIEXUQ&list=PL-osiE80TeTs4UjLw5MM6OjgkjFeUxCYH&index=3
 
 app = Flask(__name__)
+
+#Change to env variable later
+app.config['SECRET_KEY'] = 'e45e849025221566c85d306b4f45996a'
 
 posts = [
     {
@@ -31,6 +35,30 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        if form.email.data == 'admin@admin.com' and form.password.data == 'admin':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+
+        else:
+            flash('Login Unsuccessfull. Please check email and password', 'danger')
+
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
